7fce06547bc11982f2a73f1f6e07ad1920b408ec
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-03-28 17:35:55 +0000

    adds react states for permission, recording, audio blob. Gets audio chunks and pushes them in an audio blob

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
   async function getMedia() {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
@@ -8,18 +12,34 @@ export default function Home() {
         video: false,
       });
 
+      setHasPermission(true);
+
       const recorder = new MediaRecorder(stream);
+      const audioChunks: Blob[] = [];
+
+      
+      recorder.ondataavailable = (event) => { 
+        audioChunks.push(event.data);
+      };
+      
+      recorder.onstop = () => {
+        const finalBlob = new Blob(audioChunks, { type: recorder.mimeType });
+        setAudioBlob(finalBlob);
+        setIsRecording(false);
+        console.log("Recording stopped");
+      };
+      
       recorder.start();
+      setIsRecording(true);
       console.log("Recording started");
 
       setTimeout(() => {
         recorder.stop();
-        console.log("Recording stopped");
-
         stream.getTracks().forEach((track) => track.stop());
       }, 5000);
     } catch (err) {
       console.error("Error accessing microphone:", err);
+      setHasPermission(false);
     }
   }
 
