dfec565b130ca7dce45d651a5426e74558c86ba8
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-04-26 18:31:48 +0100

    changes db result into dicts in service

--- backend/app/database/recordings.py
+++ backend/app/database/recordings.py
@@ -11,9 +11,11 @@ def select_prompts_recordings(prompt_id):
     sql = "SELECT recording_id, person_name, created_at FROM recordings WHERE prompt_id = %s"
     try:
         conn = connect()
+        print("postgres connection opened")
         cur = conn.cursor()
         cur.execute(sql, (prompt_id, ))
         res = cur.fetchall()
+        print("recordings successfully returned from database")
         
         return res
     
@@ -27,6 +29,7 @@ def select_prompts_recordings(prompt_id):
             cur.close()
         if conn:
             conn.close()
+            print("postgres connection closed")
 
 
 def insert_recording(person_name_validated, prompt_id, file_content_type, file_size, storage_ref):
@@ -39,11 +42,13 @@ def insert_recording(person_name_validated, prompt_id, file_content_type, file_s
     
     try:
         conn = connect()
+        print("postgres connection opened")
         cur = conn.cursor()
         cur.execute(sql, values)
         res = cur.fetchone()
         conn.commit()
         if res:
+            print("recording successfully inserted into database")
             return {"recording_id": res[0], "created_at": res[1]}
     
     except (Exception, psycopg2.DatabaseError) as error:
@@ -56,4 +61,5 @@ def insert_recording(person_name_validated, prompt_id, file_content_type, file_s
             cur.close()
         if conn:
             conn.close()
+            print("postgres connection closed")
         
--- backend/app/services/get_service.py
+++ backend/app/services/get_service.py
@@ -1,7 +1,11 @@
-
+from database import select_prompts_recordings
 
 def list_recordings_by_prompt(prompt_id):
-    pass
+    db_res = select_prompts_recordings(prompt_id)
+    recordings = [{"recording_id": str(db_res.UUID), "person_name": db_res.person_name, "created_at": db_res.created_at} for r in db_res]
+    return recordings        
+
+    
 
 def show_persons_recording(prompt_id, prompt_name):
     pass
