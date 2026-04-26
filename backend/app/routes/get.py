c5eb6679238c3d5fb1d23afc629c45551b802973
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-04-26 15:08:35 +0100

    fixes bug with get url naming

--- backend/app/routes/get.py
+++ backend/app/routes/get.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Form
 from services import get_service
 
-router = APIRouter(prefix="/get", tags=["get"])
+router = APIRouter(tags=["get"])
 
 
 @router.get("/health")
@@ -9,11 +9,11 @@ async def health():
     return {"status": "ok"}
 
 
-@router.get("/prompts/{prompt_id}/recordings")
+@router.get("/recordings/{prompt_id}")
 # Gets all recordings for a specified prompt
 def get_recordings_by_prompt(prompt_id: int ):
     try:
-        get_service.list_recordings_by_prompt(prompt_id)
+        return get_service.list_recordings_by_prompt(prompt_id)
 
     except ValueError as err:
         raise HTTPException(status_code=400, detail=str(err))
@@ -21,7 +21,7 @@ def get_recordings_by_prompt(prompt_id: int ):
 @router.get("/prompts/{prompt_id}/{person_name}")
 def get_persons_recording(prompt_id: int, person_name: str):
     try:
-        get_service.list_recordings_by_prompt(prompt_id, person_name)
+        return get_service.list_recordings_by_prompt(prompt_id, person_name)
 
     except ValueError as err:
         raise HTTPException(status_code=400, detail=str(err))
