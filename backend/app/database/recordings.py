50a06ca94b60c20a79e5f2fcec2f9c1ad5899ae9
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-04-26 10:43:24 +0100

    fixes sql insert, returns values to service and handles errors

--- backend/app/database/recordings.py
+++ backend/app/database/recordings.py
@@ -6,12 +6,25 @@ def connect():
 
 
 def insert_recordings(person_name_validated, prompt_id, file_content_type, file_size, storage_ref):
-    sql = f"INSERT INTO recordings(prompt_id, person_name, content_type, size_bytes, storage_ref), VALUES({prompt_id}, {person_name_validated}, {file_content_type}, {file_size}, {storage_ref})"
+    sql = "INSERT INTO recordings(prompt_id, person_name, content_type, size_bytes, storage_ref) VALUES(%s, %s, %s, %s, %s) RETURNING recording_id, created_at;", ({prompt_id}, {person_name_validated}, {file_content_type}, {file_size}, {storage_ref}) 
+
+    
     
     try:
         conn = connect()
         cur = conn.cursor()
         cur.execute(sql)
+        res = cur.fetchone()
+        
+        
+    
         conn.commit()
+        return res
+    
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        conn.rollback()
+        return error
+
+    finally:
+        cur.close()
+        conn.close()
