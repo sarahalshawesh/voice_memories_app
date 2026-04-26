00ed80a5d8fd17c82c0c2cf3464418dc168e19fe
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-04-26 10:53:04 +0100

    Sets conn and cur as None for error handling if conn fails

--- backend/app/database/recordings.py
+++ backend/app/database/recordings.py
@@ -5,26 +5,31 @@ def connect():
     return psycopg2.connect(DB_CONNECTION_STRING)
 
 
-def insert_recordings(person_name_validated, prompt_id, file_content_type, file_size, storage_ref):
-    sql = "INSERT INTO recordings(prompt_id, person_name, content_type, size_bytes, storage_ref) VALUES(%s, %s, %s, %s, %s) RETURNING recording_id, created_at;", ({prompt_id}, {person_name_validated}, {file_content_type}, {file_size}, {storage_ref}) 
+def insert_recording(person_name_validated, prompt_id, file_content_type, file_size, storage_ref):
+    conn, cur = None, None
+    sql = "INSERT INTO recordings(prompt_id, person_name, content_type, size_bytes, storage_ref) VALUES(%s, %s, %s, %s, %s) RETURNING recording_id, created_at;"
+    
+    values = (prompt_id, person_name_validated, file_content_type, file_size, storage_ref) 
 
     
     
     try:
         conn = connect()
         cur = conn.cursor()
-        cur.execute(sql)
+        cur.execute(sql, values)
         res = cur.fetchone()
         
         
     
         conn.commit()
-        return res
+        return {"recording_id": res[0], "created_at": res[1]}
     
     except (Exception, psycopg2.DatabaseError) as error:
         conn.rollback()
-        return error
+        raise error
 
     finally:
-        cur.close()
-        conn.close()
+        if conn:
+            conn.close()
+        if cur:
+            cur.close()
