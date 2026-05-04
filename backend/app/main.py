096c0163e221d1a93547d074204f3ce75b4c123f
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-05-04 20:41:53 +0100

    adds render url to origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,8 @@ origins = [
     "http://127.0.0.1:3000", 
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://blether.vercel.app"
+    "https://blether.vercel.app",
+    "https://remember-me-jzba.onrender.com/upload/"
 ]
 app.add_middleware(CORSMiddleware, allow_origins = origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
